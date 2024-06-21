065222f97e28bfdb9dae2317dba51fb872d568af
Gianobaldo <134638468+Gianobaldo@users.noreply.github.com>
2024-06-21 14:37:17 -0400

    Create OperatorPrecedence.py

---
+++ Python-Basics/OperatorPrecedence.py
@@ -0,0 +1,11 @@
+#Guess the output of each statement before clicking run
+
+print((5 + 4) * 10 / 2) #45
+
+print(((5 + 4) * 10) / 2) #45
+
+print((5 + 4) * (10 / 2)) #45
+
+print(5 + (4 * 10) / 2) #25
+
+print(5 + 4 * 10 // 2) #25
