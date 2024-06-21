33e59b3b6a43272e6132a32c10cf98773aa05905
Gianobaldo <134638468+Gianobaldo@users.noreply.github.com>
2024-06-21 14:56:13 -0400

    Create StringIndexes.py

---
+++ Python-Basics/StringIndexes.py
@@ -0,0 +1,12 @@
+#Guess the output of each print statement before running them.
+python = 'I am PYHTON'
+
+print(python[1:4]) # am 
+print(python[1:])
+print(python[:])
+print(python[1:100])
+print(python[-1]) #N
+print(python[-4]) #H
+print(python[:-3]) #I am PYH
+print(python[-3:]) #TON
+print(python[::-1]) #ONTHYP ma I
