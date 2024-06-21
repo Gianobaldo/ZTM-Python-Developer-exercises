bb49f9d492f1d39766f166b518d87de83b629e31
Gianobaldo <134638468+Gianobaldo@users.noreply.github.com>
2024-06-21 19:49:55 -0400

    Update Functions.py

--- 3.-Python-Basics2/Functions.py
+++ 3.-Python-Basics2/Functions.py
@@ -1,1 +1,9 @@
+#write a function to find the highest even number in a list
+def highest_even(lis)
+evens= []
+for nums in lis:
+  if not item in lis and item not in evens:
+    evens.append(item)
+return max(evens)
 
+print(highest_even([1,2,3,4,5,6,8])
